50ae729e3c339be7f728a4cdf6618610ba90ae61
Somebro <someBroYouKnow@users.noreply.github.com>
2025-10-06 04:38:29 +0530

    day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div style={{ position: "relative", width: "1920px", height: "1080px" }}>
-      <iframe
-        src="https://player.cloudinary.com/embed/?cloud_name=dzyg5bubn&public_id=Veefin_Intro_Screen_t9fbf5&profile=cld-looping&autoplay=true&controls=false"
-        width="1920"
-        height="1080"
-        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
-        style={{ border: "none" }}
-      ></iframe>
+      <video
+  src="https://res.cloudinary.com/dzyg5bubn/video/upload/v1759668050/Veefin_Intro_Screen_ryhw8x.mp4"
+  width="1920"
+  height="1080"
+  autoPlay
+  muted
+  loop
+  playsInline
+  style={{ display: "block", width: "100%", height: "100%", objectFit: "cover" }}
+/>
       <Link
         style={{
           position: "absolute",
